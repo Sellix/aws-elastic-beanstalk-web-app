c0bd8d691b885cb4c2207c6b38018b0d1f7049f8
coval3nte <65908512+coval3nte@users.noreply.github.com>
2023-05-02 20:45:07 +0200

    add beanstalk app version lifecycle

--- beanstalk/beanstalk.tf
+++ beanstalk/beanstalk.tf
@@ -124,4 +124,10 @@ resource "aws_elastic_beanstalk_environment" "sellix-eb-environment" {
 resource "aws_elastic_beanstalk_application" "sellix-eb" {
   name        = var.tags["Project"]
   description = "NodeJS Web Application"
+  
+  appversion_lifecycle {
+    service_role          = aws_iam_role.sellix-eb-service-role.arn
+    max_count             = 10
+    delete_source_from_s3 = true
+  }
 }
